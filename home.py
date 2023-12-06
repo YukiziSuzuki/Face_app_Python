6bbdc2b48d695e1ebc290fb32b6cda933c1fe057
YukiziSuzuki <kurokimadousi@gmail.com>
2023-12-06 19:16:42 +0900

    add

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("入退室管理システム")
+
+    # ここにホーム画面のコンテンツを追加
+    st.write("ようこそ！ここは多胡研究室の入退室管理システムです")
+    st.write("画面左のメニューから操作を選択してください")
+
+if __name__ == "__main__":
+    main()
